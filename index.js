8bf26cff08678d46ecbe22b7f544b6aae83603a9
richie0x <126347448+richie0x@users.noreply.github.com>
2023-05-08 19:01:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var image = document.getElementById("title-img-1");
+
+setTimeout(function() {
+  image.style.bottom = "0";
+  image.style.left ="0";
+}, 400)
+
+var image2 = document.getElementById("title-img-2");
+
+setTimeout(function() {
+  image2.style.bottom = "0";
+  image2.style.right = "0"
+}, 400)
